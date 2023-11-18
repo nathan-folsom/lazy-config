13d36f318cc119c1306f2dac60a61c5f5c415e95
Nathan Folsom <n8folsom@gmail.com>
2023-11-18 12:30:02 -0800

    disable autopair for single quote after an angle bracket for rust lifetimes

---
+++ lua/plugins/pairs.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "echasnovski/mini.pairs",
+    opts = {
+      mappings = {
+        ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\<].", register = { cr = false } },
+      },
+    },
+  },
+}
