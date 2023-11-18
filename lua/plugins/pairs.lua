840050dcae5aa88ffe66890028f595dd97400e51
Nathan Folsom <n8folsom@gmail.com>
2023-11-18 12:55:32 -0800

    add autopairs for angle brackets

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -4,6 +4,8 @@ return {
     opts = {
       mappings = {
         ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\<&].", register = { cr = false } },
+        ["<"] = { action = "open", pair = "<>", neigh_pattern = "[^\\]." },
+        [">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
       },
     },
   },
