e51956c74b772a3b927183fb723d1afee0fb6f6b
Nathan Folsom <n8folsom@gmail.com>
2023-11-20 17:57:33 -0800

    disable autopair for angle bracket after space for comparison operators

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       mappings = {
         ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\<&].", register = { cr = false } },
-        ["<"] = { action = "open", pair = "<>", neigh_pattern = "[^\\]." },
+        ["<"] = { action = "open", pair = "<>", neigh_pattern = "[^\\]. " },
         [">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
       },
     },
