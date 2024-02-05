b3f9b51bd0c07ccd9451371ca91c52214a321591
Nate Folsom <nate@rhombussystems.com>
2024-02-05 14:55:28 -0800

    Backtick pair <CR> handling

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       mappings = {
         ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\<&].", register = { cr = false } },
+        ["`"] = { action = "closeopen", pair = "``", register = { cr = true } },
         ["<"] = { action = "open", pair = "<>", neigh_pattern = "%w.", register = { cr = false } },
         [">"] = { action = "close", pair = "<>", register = { cr = false } },
       },
