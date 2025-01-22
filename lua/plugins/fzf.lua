93ff3d146ce4456fd6aeb4ba84161c912c3ec05f
Nathan Folsom <nate@Nathans-MacBook-Air.local>
2025-01-21 21:20:56 -0800

    change live grep to use cwd instead of root

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -6,6 +6,7 @@ return {
       { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
       { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
       { "<leader><leader>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (Root Dir)" },
     },
     opts = {
       defaults = {
