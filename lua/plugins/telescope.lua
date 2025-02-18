9bf1670579e8bd76e5d76b9ed6713c207b3efb60
Nate Folsom <nate@rhombussystems.com>
2025-02-18 13:03:29 -0800

    Add fzf keybinds to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,5 +6,12 @@ return {
         layout_strategy = "vertical",
       },
     },
+    keys = {
+      -- Swap keymaps for file finder, I pretty much always use the cwd version
+      { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+      { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      { "<leader><leader>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+    },
   },
 }
