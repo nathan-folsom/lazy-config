2854afe72b3ef649ea7c4d5017f5f2990442df1b
Nate Folsom <nate@rhombussystems.com>
2025-10-06 17:04:21 -0700

    disable fzf, getting errors and it's unneeded because I use telescope

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,18 +1,18 @@
 return {
-  {
-    "fzf-lua",
-    keys = {
-      -- Swap keymaps for file finder, I pretty much always use the cwd version
-      { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
-      { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
-      { "<leader><leader>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
-      { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
-    },
-    opts = {
-      defaults = {
-        -- Was getting some warnings about this slowing things down
-        git_icons = false,
-      },
-    },
-  },
+  -- {
+  --   "fzf-lua",
+  --   keys = {
+  --     -- Swap keymaps for file finder, I pretty much always use the cwd version
+  --     { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+  --     { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+  --     { "<leader><leader>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+  --     { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+  --   },
+  --   opts = {
+  --     defaults = {
+  --       -- Was getting some warnings about this slowing things down
+  --       git_icons = false,
+  --     },
+  --   },
+  -- },
 }
