22bd7104eaaa3e91039e456ab227900d7bfdaa2a
Nate Folsom <nate@rhombussystems.com>
2025-10-07 12:16:18 -0700

    neo-tree config
    - show hidden items by default
    - swap root and cwd comands

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,6 +3,28 @@ return {
     "neo-tree.nvim",
     opts = {
       window = {},
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+    keys = {
+      -- Swap root and cwd commands since I pretty much always use cwd
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
     },
   },
 }
