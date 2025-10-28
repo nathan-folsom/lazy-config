6216200a9a627d126f1b7480fc82d9418aec1e5c
Nate Folsom <nate@rhombussystems.com>
2025-10-28 10:43:59 -0700

    update deprecated api usage, enable flow lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,18 @@
 return {
   "neovim/nvim-lspconfig",
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = {
-      "gr",
-      function()
-        require("telescope.builtin").lsp_references({ show_line = false })
-      end,
-    }
-  end,
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          {
+            "gd",
+            function()
+              require("telescope.builtin").lsp_references({ show_line = false })
+            end,
+          },
+        },
+      },
+      flow = { enabled = true },
+    },
+  },
 }
