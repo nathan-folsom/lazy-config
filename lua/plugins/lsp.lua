83094f7ca255a7513f77b744c3a1061dc0213b81
Nate Folsom <nate@rhombussystems.com>
2025-10-28 12:26:10 -0700

    only use correct lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,7 +12,28 @@ return {
           },
         },
       },
-      flow = { enabled = true },
+      flow = {
+        enabled = true,
+        cmd = {
+          "npx",
+          "flow",
+          "lsp",
+          "--flowconfig-name",
+          "scripts/flow/custom/.flowconfig",
+        },
+      },
+    },
+    setup = {
+      flow = function()
+        Snacks.util.lsp.on(function(_, client)
+          local is_react = vim.fn.getcwd() == "/Users/nate/rhombus/react"
+          if is_react and client.name == "typescript-tools" then
+            client:stop(true)
+          elseif not is_react and client.name == "flow" then
+            client:stop(true)
+          end
+        end)
+      end,
     },
   },
 }
