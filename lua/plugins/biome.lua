c840bbc1f47a935aa46472e6ada914be603e393a
Nate Folsom <nate@rhombussystems.com>
2025-11-20 13:27:01 -0800

    switch back to normal tsserver, typescript tools not starting up

--- lua/plugins/biome.lua
+++ lua/plugins/biome.lua
@@ -8,20 +8,12 @@ return {
           Snacks.util.lsp.on(function(buf, client)
             if client.name == "biome" then
               client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "typescript-tools" then
+            elseif client.name == "typescript-tools" or client.name == "ts_ls" then
               client.server_capabilities.documentFormattingProvider = false
             elseif client.name == "eslint" then
               client:stop(true)
             end
           end)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            callback = function()
-              vim.lsp.buf.code_action({
-                context = { only = { "source.fixAll.biome" } },
-                apply = true,
-              })
-            end,
-          })
         end,
       },
     },
--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {
-      root_dir = require("lspconfig.util").root_pattern(".git"),
-      settings = {
-        tsserver_plugins = {
-          "@styled/typescript-styled-plugin",
-        },
-      },
-    },
-  },
+  -- {
+  --   "pmizio/typescript-tools.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  --   opts = {
+  --     root_dir = require("lspconfig.util").root_pattern(".git"),
+  --     settings = {
+  --       tsserver_plugins = {
+  --         "@styled/typescript-styled-plugin",
+  --       },
+  --     },
+  --   },
+  -- },
 }
